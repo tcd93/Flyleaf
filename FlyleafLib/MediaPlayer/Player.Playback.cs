1c92336a27ed5897ea7ab178990a644dcd808012
tcd93 <dungtang93@gmail.com>
2022-03-18 19:54:25 +0700

    Remove OnPlayback event firing from Pausing video
    FlyleafAudioPlayer target NET 6.0 (to suppress warnings on VS2022 - but it does not on VS2019)

--- FlyleafLib/MediaPlayer/Player.Playback.cs
+++ FlyleafLib/MediaPlayer/Player.Playback.cs
@@ -122,7 +122,6 @@ namespace FlyleafLib.MediaPlayer
                                 status = Status.Paused;
                         }
 
-                        OnPlaybackCompleted(stoppedWithError ? "Playback stopped unexpectedly" : null);
                         if (CanDebug) Log.Debug($"[SCREAMER] Finished (Status: {Status}, Error: {(stoppedWithError ? "Playback stopped unexpectedly" : "")})");
 
                         UI(() =>
